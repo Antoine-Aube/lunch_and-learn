748f0a04cc6b81cbafe37cc221529f1e1ec87b30
Antoine Aube <aaube3@gmail.com>
2023-11-11 14:43:27 -0700

    test: recipe service test

---
+++ spec/services/recipes_service_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec. describe RecipesService do 
+  let(:service) { RecipesService.new}
+
+  describe "#get_recipes" do
+    it "returns a successfule response with correct json" do 
+      query = "Vietnam"
+      response = service.get_recipes(query)
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(200)
+      expect(response_body).to be_a(Hash)
+    end
+  end
+end
