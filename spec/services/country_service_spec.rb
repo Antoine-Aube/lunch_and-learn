699ead7cb1b44460919feb00e82bed7719428a44
Antoine Aube <aaube3@gmail.com>
2023-11-11 15:56:19 -0700

    test: country service test

---
+++ spec/services/country_service_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe CountryService do
+  let(:service) { CountryService.new }
+
+  describe "#sample_country", :vcr do
+    it "returns a successful response with correct json" do
+      response = service.sample_country
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(200)
+      expect(response_body[0]).to be_a(Hash)
+    end
+  end
+
+
+end
