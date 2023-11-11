2a47e75ea6a28c0002e4735e2fd123fce1407b05
Antoine Aube <aaube3@gmail.com>
2023-11-11 10:16:42 -0700

    test: intial text to get successful response from edamam

---
+++ spec/requests/api/v1/recipes_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "recipes endpoints" do 
+  describe "GET /api/v1/recipes" do
+    it "returns a successful response" do 
+      get "/api/v1/recipes", params: {q: "Vietnam"}
+
+      expect(response).to be_successful
+    end
+  end
+end
