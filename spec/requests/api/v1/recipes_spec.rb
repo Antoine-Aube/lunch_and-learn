fe6f84b746fe563a13c15800f7820b4add06bde5
Antoine Aube <aaube3@gmail.com>
2023-11-11 10:20:30 -0700

    docs: all remaining files - serializers

--- spec/requests/api/v1/recipes_spec.rb
+++ spec/requests/api/v1/recipes_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "recipes endpoints" do 
   describe "GET /api/v1/recipes" do
     it "returns a successful response" do 
-      get "/api/v1/recipes", params: {q: "Vietnam"}
+      get "/api/v1/recipes", params: {country: "Vietnam"}
 
       expect(response).to be_successful
     end
