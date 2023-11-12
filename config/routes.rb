bcdc4816790f84f2f9132f7e20da37d7c9c1f4a8
Antoine Aube <aaube3@gmail.com>
2023-11-11 20:03:02 -0700

    update credentials and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/api/v1/recipes", to: "api/v1/recipes#index"
-  get "api/v1/learning_resources", to: "api/v1/learning_resources#index"
+  get "api/v1/learning_resources", to: "api/v1/learning_resources#show"
 end
