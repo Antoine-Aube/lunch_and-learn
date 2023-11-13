81944a02370d400d508d84a67ae6c77277676c41
Antoine Aube <aaube3@gmail.com>
2023-11-13 11:20:57 -0700

    refactor: json parsing for SRP + Encapsulation

--- app/facades/tourist_sites_facade.rb
+++ app/facades/tourist_sites_facade.rb
@@ -2,7 +2,7 @@ class TouristSitesFacade
   def self.get_tourist_sites(country)
     coordinates = get_capital_coordinates(country)
     response = PlacesService.new.get_tourist_sites(coordinates)
-    response_body = JSON.parse(response.body, symbolize_names: true)
+    response_body = parse_json(response)
     if response_body[:features].empty?
       return nil
     else
@@ -14,12 +14,18 @@ class TouristSitesFacade
 
   def self.get_capital_coordinates(country)
     response = CountryService.new.capital_coordinates(country)
-    response_body = JSON.parse(response.body, symbolize_names: true)
+    response_body = parse_json(response)
     country_info = response_body.first
     if country_info[:capitalInfo].empty?
       [0,0]
     else 
       country_info[:capitalInfo][:latlng]
     end
+  end
+
+  private
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
