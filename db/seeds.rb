9d88530df7c6005a0a2ab54d8077172c25b19b5a
Antoine Aube <aaube3@gmail.com>
2023-11-13 14:23:24 -0700

    add seed for second user with no favorites to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 user1 = User.create(name: "Craig", email: "craig@gmail.com", password: "password")
+user2 = User.create(name: "Vanessa", email: "Vanessa@gmail.com", password: "password")
