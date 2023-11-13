662cd972346894a3798abf0a0b69880c8b532979
Antoine Aube <aaube3@gmail.com>
2023-11-13 11:02:41 -0700

    routes: for tourist site endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/api/v1/recipes", to: "api/v1/recipes#index"
+  get "/api/v1/tourist_sites", to: "api/v1/tourist_sites#index"
 end
