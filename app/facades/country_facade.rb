899ae963e7741ec2d5f95afad257fe4912fe6cfb
Antoine Aube <aaube3@gmail.com>
2023-11-13 11:49:01 -0700

    final commit

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -3,7 +3,6 @@ class CountryFacade
     response = CountryService.new.sample_country
     response_body = JSON.parse(response.body, symbolize_names: true)
     response_body.map do |country_data|
-      # require 'pry';binding.pry
       Country.new(country_data)
     end
   end
