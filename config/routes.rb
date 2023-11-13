4586250ae39e99bf1c4959bad781d960af9db872
Antoine Aube <aaube3@gmail.com>
2023-11-13 13:35:12 -0700

    route for user favorite create

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/api/v1/learning_resources", to: "api/v1/learning_resources#show"
   post "/api/v1/users", to: "api/v1/users#create"
   post "/api/v1/sessions", to: "api/v1/sessions#create"
+  post "/api/v1/favorites", to: "api/v1/favorites#create"
 end
